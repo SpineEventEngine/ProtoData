cdd364ff18c47c6cf646c605c08c70b53fa2beaf
Serhii Lekariev <68739556+sae3023@users.noreply.github.com>
2021-04-21 20:41:35 +0300

    Fix a typo.

--- compiler/src/main/kotlin/io/spine/protodata/Pipeline.kt
+++ compiler/src/main/kotlin/io/spine/protodata/Pipeline.kt
@@ -39,7 +39,7 @@ import io.spine.server.storage.memory.InMemoryStorageFactory
  * A pipeline which processes the Protobuf files.
  *
  * A pipeline consists of the `Code Generation` context, which receives Protobuf compiler events,
- * and one ofr more [Renderer]s. A pipeline runs on a single source set.
+ * and one or more [Renderer]s. A pipeline runs on a single source set.
  *
  * The pipeline starts by building the `Code Generation` bounded context with the supplied
  * [Plugin]s. Then, the Protobuf compiler events are emitted and the subscribers in
