8716ab509519daf05d6f920a43a676abdda3f3ac
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-10-04 15:45:54 +0300

    Simplify path construction in Gradle extension

--- gradle-plugin/src/main/kotlin/io/spine/protodata/gradle/Extension.kt
+++ gradle-plugin/src/main/kotlin/io/spine/protodata/gradle/Extension.kt
@@ -100,7 +100,7 @@ public class Extension(private val project: Project) {
 
     internal val requestFilesDirProperty: DirectoryProperty =
         project.objects.directoryProperty().convention(
-            project.layout.buildDirectory.dir("protodata").map { it.dir("requests") }
+            project.layout.buildDirectory.dir("protodata/requests")
         )
 
     internal fun requestFile(forSourceSet: SourceSet): Provider<RegularFile> =
