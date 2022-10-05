2f0bcf78fd820e3dc6975022719874e6cc75ab15
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-10-05 13:12:26 +0300

    Update the "dev" version.

--- version.gradle.kts
+++ version.gradle.kts
@@ -31,7 +31,7 @@ val mcVersion: String by extra("2.0.0-SNAPSHOT.90")
 val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.99")
 
 /** The version of ProtoData used for developing [protoDataVersion]. */
-val devProtoDataVersion: String by extra("0.2.12")
+val devProtoDataVersion: String by extra("0.2.15")
 
 // The version of ProtoData being developed.
 val protoDataVersion: String by extra("0.2.16")
