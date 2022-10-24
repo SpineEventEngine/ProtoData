8938c03e897c8b0488ee6b094ee9115065b05f7a
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-24 22:05:00 +0300

    Bump version -> `0.2.21`
    ... together with:
      * `base` -> `2.0.0-SNAPSHOT.115`
      * `core` -> `2.0.0-SNAPSHOT.117`

--- version.gradle.kts
+++ version.gradle.kts
@@ -24,11 +24,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val baseVersion: String by extra("2.0.0-SNAPSHOT.114")
-val coreVersion: String by extra("2.0.0-SNAPSHOT.116")
+val baseVersion: String by extra("2.0.0-SNAPSHOT.115")
+val coreVersion: String by extra("2.0.0-SNAPSHOT.117")
 val toolBaseVersion: String by extra("2.0.0-SNAPSHOT.111")
 val mcVersion: String by extra("2.0.0-SNAPSHOT.90")
 val mcJavaVersion: String by extra("2.0.0-SNAPSHOT.102")
 
 // The version of ProtoData being developed.
-val protoDataVersion: String by extra("0.2.20")
+val protoDataVersion: String by extra("0.2.21")
