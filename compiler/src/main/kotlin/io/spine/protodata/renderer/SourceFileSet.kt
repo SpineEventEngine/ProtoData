41b41ca9ed4722f77e48804fae64f44edc69a203
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-11-10 19:39:24 +0300

    Pull utility `fun` out

--- compiler/src/main/kotlin/io/spine/protodata/renderer/SourceFileSet.kt
+++ compiler/src/main/kotlin/io/spine/protodata/renderer/SourceFileSet.kt
@@ -106,19 +106,6 @@ internal constructor(
         @VisibleForTesting
         internal fun from(sourceAndTarget: Path): SourceFileSet =
             from(sourceAndTarget, sourceAndTarget)
-
-        private fun checkTarget(targetRoot: Path) {
-            if (targetRoot.exists()) {
-                val target = targetRoot.toFile()
-                checkArgument(
-                    target.isDirectory, "Target root `%s` must be a directory.", targetRoot
-                )
-                val children = target.list()!!
-                checkArgument(children.isEmpty(),
-                    "Target directory `%s` must be empty. Found children: %s.",
-                    targetRoot, children.joinToString())
-            }
-        }
     }
 
     /**
@@ -138,8 +125,9 @@ internal constructor(
      */
     public fun file(path: Path): SourceFile =
         findFile(path).orElseThrow {
-            IllegalArgumentException("File not found: `$path`." +
-                    " Source root: `$sourceRoot`. Target root: `$targetRoot`.")
+            error(
+                "File not found: `$path`. Source root: `$sourceRoot`. Target root: `$targetRoot`."
+            )
         }
 
     /**
@@ -236,4 +224,17 @@ internal constructor(
 
 private fun Path.canonical(): Path {
     return toAbsolutePath().normalize()
+}
+
+private fun checkTarget(targetRoot: Path) {
+    if (targetRoot.exists()) {
+        val target = targetRoot.toFile()
+        checkArgument(
+            target.isDirectory, "Target root `%s` must be a directory.", targetRoot
+        )
+        val children = target.list()!!
+        checkArgument(children.isEmpty(),
+            "Target directory `%s` must be empty. Found children: %s.",
+            targetRoot, children.joinToString())
+    }
 }
