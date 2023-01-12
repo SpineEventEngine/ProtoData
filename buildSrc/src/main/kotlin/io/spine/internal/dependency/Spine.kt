1e0eb10e069e1d187612d8203826c19aebeecaa3
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-12 14:13:58 +0000

    Bump `tool-base` and `ProtoData`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -61,7 +61,7 @@ class Spine(p: ExtensionAware) {
          *
          * @see [ProtoData]
          */
-        const val protoData = "0.5.3"
+        const val protoData = "0.5.4"
 
         /**
          * The default version of `base` to use.
@@ -116,7 +116,7 @@ class Spine(p: ExtensionAware) {
          * The version of `tool-base` to use.
          * @see [Spine.toolBase]
          */
-        const val toolBase = "2.0.0-SNAPSHOT.152"
+        const val toolBase = "2.0.0-SNAPSHOT.154"
 
         /**
          * The version of `validation` to use.
