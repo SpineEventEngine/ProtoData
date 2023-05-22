7f3bfe18aa32c95a303565af46c18d1a84f046cf
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-05-22 18:22:40 +0100

    Temporarily apply additional filtering

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -30,6 +30,7 @@ import io.spine.internal.dependency.Protobuf
 import io.spine.internal.dependency.Spine
 import io.spine.internal.dependency.Truth
 import io.spine.internal.gradle.kotlin.setFreeCompilerArgs
+import io.spine.internal.gradle.protobuf.excludeProtocOutput
 import io.spine.internal.gradle.standardToSpineSdk
 import io.spine.internal.gradle.testing.configureLogging
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -73,6 +74,9 @@ subprojects {
     protobuf {
         protoc {
             artifact = Protobuf.compiler
+        }
+        generateProtoTasks.all().configureEach {
+            excludeProtocOutput()
         }
     }
 
