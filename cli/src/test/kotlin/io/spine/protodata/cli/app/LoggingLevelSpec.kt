089e242d81d9ba1bafbd10f78489e1bad5b51ad1
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-05-30 00:21:33 +0100

    Remove the delay
    ... because it gives nothing.

--- cli/src/test/kotlin/io/spine/protodata/cli/app/LoggingLevelSpec.kt
+++ cli/src/test/kotlin/io/spine/protodata/cli/app/LoggingLevelSpec.kt
@@ -129,7 +129,6 @@ private fun tapConsole(block: () -> Unit): String {
 
     stream.use {
         try {
-            Thread.sleep(1000)
             block()
             bytes.flush()
             stream.flush()
