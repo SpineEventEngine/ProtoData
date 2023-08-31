8b4baa80db0f86850f07d1721fd8eea6af85b326
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-09-01 00:13:42 +0100

    Pull `ClassName` and `JavaElement` out
    Also:
      * Expose extension functions for `File`.

---
+++ codegen/java/src/main/kotlin/io/spine/protodata/codegen/java/ClassName.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.protodata.codegen.java
+
+import io.spine.protodata.type.TypeNameElement
+import io.spine.tools.code.Java
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.reflect.KClass
+
+/**
+ * A fully qualified Java class name.
+ *
+ * In Java, a class name is not a valid expression. Use one of the methods of this class to create
+ * an expression from this class name.
+ */
+public class ClassName
+internal constructor(
+    private val packageName: String,
+    private val simpleNames: List<String>
+) : JavaElement, TypeNameElement<Java> {
+
+    /**
+     * The canonical name of the class.
+     *
+     * This is the name by which the class is referred to in Java code.
+     *
+     * For regular Java classes, This is similar to [binary], except that in a binary name nested
+     * classes are separated by the dollar (`$`) sign, and in canonical — by the dot (`.`) sign.
+     */
+    @get:JvmName("canonical")
+    public val canonical: String = "$packageName.${simpleNames.joinToString(".")}"
+
+    /**
+     * The binary name of the class.
+     *
+     * This is the name by which the class is referred to in Bytecode.
+     *
+     * For regular Java classes, This is similar to [canonical], except that in a binary name nested
+     * classes are separated by the dollar (`$`) sign, and in canonical — by the dot (`.`) sign.
+     */
+    @get:JvmName("binary")
+    public val binary: String
+        get() = "$packageName.${simpleNames.joinToString("$")}"
+
+    @get:JvmName("javaFile")
+    public val javaFile: Path by lazy {
+        val dir = packageName.replace('.', '/')
+        val topLevelClass = simpleNames.first()
+        Path("$dir/$topLevelClass.java")
+    }
+
+    /**
+     * The simple name of this class.
+     *
+     * If the class is nested inside another class, the outer class name is NOT included.
+     */
+    @get:JvmName("simpleName")
+    public val simpleName: String
+        get() = simpleNames.last()
+
+    /**
+     * Obtains the class name of the given Java class.
+     */
+    public constructor(cls: Class<*>) : this(cls.`package`.name, cls.names())
+
+    /**
+     * Obtains the Java class name of the given Kotlin class.
+     */
+    public constructor(klass: KClass<*>) : this(klass.java)
+
+    override fun toCode(): String = canonical
+
+    override fun toString(): String = canonical
+}
+
+/**
+ * Obtains the simple name of this class including the names of the declaring classes.
+ */
+private fun Class<*>.names(): List<String> {
+    if (declaringClass == null) {
+        return listOf(this.simpleName)
+    }
+    val names = mutableListOf<String>()
+    var cls: Class<*>? = this
+    do {
+        names.add(cls!!.simpleName)
+        cls = cls.declaringClass
+    } while (cls != null)
+    return names.reversed()
+}
