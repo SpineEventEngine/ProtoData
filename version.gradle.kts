f05d9b45b48087dee219aed277e1aae123a33874
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-01-14 23:06:09 +0000

    Bump version -> `0.17.0`

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,4 +32,4 @@
  *
  * For dependencies on Spine SDK module please see [io.spine.internal.dependency.Spine].
  */
-val protoDataVersion: String by extra("0.16.3")
+val protoDataVersion: String by extra("0.17.0")
