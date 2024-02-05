fba4c6c9ee50c0950ed9c28055f32f9a578909eb
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-02-05 21:51:19 +0000

    Bump `base` and `tool-base`

--- buildSrc/src/main/kotlin/module.gradle.kts
+++ buildSrc/src/main/kotlin/module.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -99,6 +99,8 @@ fun Module.forceConfigurations() {
         resolutionStrategy {
             force(
                 Protobuf.compiler,
+                Spine.base,
+                Spine.toolBase,
             )
         }
     }
