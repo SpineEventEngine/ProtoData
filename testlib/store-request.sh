ebefc752942dd2d13ff8fb90486d50fb01f791f6
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-04-25 02:47:33 +0100

    Fix the path to stored request

--- testlib/store-request.sh
+++ testlib/store-request.sh
@@ -11,11 +11,10 @@
 # Get the directory in which the script resides. It's the root of the subproject.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# We put the file under the `test/resources` so that tests can pick it up.
-TARGET_DIR="$SCRIPT_DIR/build/test/resources"
-mkdir -p $TARGET_DIR
+# We put the file under the `build/resources/test` so that tests can pick it up.
+TARGET_DIR="$SCRIPT_DIR/build/resources/test/codegen-request"
+mkdir -p "$TARGET_DIR"
 
 OUTPUT_FILE="$TARGET_DIR/CodeGeneratorRequest.binpb"
 
 cat >"$OUTPUT_FILE"
-
