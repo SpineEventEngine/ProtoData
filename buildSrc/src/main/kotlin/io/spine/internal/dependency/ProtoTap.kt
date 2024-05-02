99ecccb74bf5066595e9a387804842247e5c984d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-05-02 18:08:28 +0100

    Add `prototap` build extension
    Also:
     * Fix doc language grammar and punctuation.

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoTap.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoTap.kt
@@ -24,23 +24,23 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import io.spine.internal.dependency.ProtoTap
-import io.spine.internal.dependency.Spine
+package io.spine.internal.dependency
 
-plugins {
-    protobuf
-    `java-test-fixtures`
-    prototap
-}
-
-prototap {
-    artifact.set(io.spine.internal.dependency.Protobuf.compiler)
-}
-
-dependencies {
-    api(Spine.testlib)
-    api(Spine.CoreJava.testUtilServer)
-    api(ProtoTap.api)
-    api(project(":api"))
-    api(project(":backend"))
+/**
+ * Dependencies on ProtoTap plugins.
+ *
+ * See [`SpineEventEngine/ProtoTap`](https://github.com/SpineEventEngine/ProtoTap/).
+ */
+@Suppress(
+    "unused" /* Some subprojects do not use ProtoData directly. */,
+    "ConstPropertyName" /* We use custom convention for artifact properties. */,
+    "MemberVisibilityCanBePrivate" /* The properties are used directly by other subprojects. */,
+)
+object ProtoTap {
+    const val group = "io.spine.tools"
+    const val version = "0.8.1"
+    const val gradlePluginId = "io.spine.prototap"
+    const val api = "$group:prototap-api:$version"
+    const val gradlePlugin = "$group:prototap-gradle-plugin:$version"
+    const val protocPlugin = "$group:prototap-protoc-plugin:$version"
 }
