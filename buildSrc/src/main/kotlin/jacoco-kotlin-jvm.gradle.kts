bcebfd4b7eb083281b58323d8178b89aba28d774
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-05-31 15:49:12 +0100

    Update `config`

--- buildSrc/src/main/kotlin/jacoco-kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/jacoco-kotlin-jvm.gradle.kts
@@ -49,6 +49,7 @@ private val about = ""
 /**
  * Configure Jacoco task with custom input from this Kotlin Multiplatform project.
  */
+@Suppress("unused")
 val jacocoTestReport: JacocoReport by tasks.getting(JacocoReport::class) {
 
     val classFiles = File("$buildDirectory/classes/kotlin/jvm/")
