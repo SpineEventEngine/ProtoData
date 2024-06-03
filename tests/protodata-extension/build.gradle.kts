1ce1b33dbddb8095d276a499ce5a8e8999131aef
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-06-03 14:28:24 +0100

    Fix imports placement

--- tests/protodata-extension/build.gradle.kts
+++ tests/protodata-extension/build.gradle.kts
@@ -1,13 +1,11 @@
-import io.spine.internal.dependency.AutoService
-
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -25,6 +23,8 @@ import io.spine.internal.dependency.AutoService
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+import io.spine.internal.dependency.AutoService
 
 buildscript {
     standardSpineSdkRepositories()
