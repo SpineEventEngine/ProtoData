fa082a53864eac4d0dbe0f1af036a659770bea88
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-06-30 17:53:37 +0100

    Bump `reflect` -> `2.0.0-SNAPSHOT.186`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -52,7 +52,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/reflect">spine-reflect</a>
          */
-        const val reflect = "2.0.0-SNAPSHOT.184"
+        const val reflect = "2.0.0-SNAPSHOT.186"
 
         /**
          * The version of [Spine.Logging].
