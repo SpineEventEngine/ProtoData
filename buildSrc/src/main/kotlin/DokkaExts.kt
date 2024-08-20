e8f2e757f7259e55c226e853f0d1d607702d177a
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-08-20 19:36:00 +0100

    Fix doc grammar

--- buildSrc/src/main/kotlin/DokkaExts.kt
+++ buildSrc/src/main/kotlin/DokkaExts.kt
@@ -58,7 +58,7 @@ fun DependencyHandlerScope.useDokkaForKotlinAsJava() {
 
 /**
  * To exclude pieces of code annotated with `@Internal` from the documentation
- * a custom plugin is added to the Dokka's classpath.
+ * a custom plugin is added to the Dokka classpath.
  *
  * @see <a href="https://github.com/SpineEventEngine/dokka-tools/tree/master/dokka-extensions">
  *     Custom Dokka Plugins</a>
