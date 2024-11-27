52d1670672ca4b3632d5dcb302f7a41904d6527c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-27 19:51:52 +0000

    Bump ProtoData -> `0.69.2`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
@@ -73,7 +73,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.69.0"
+    private const val fallbackVersion = "0.69.2"
 
     /**
      * The distinct version of ProtoData used by other build tools.
@@ -82,7 +82,7 @@ object ProtoData {
      * transitional dependencies, this is the version used to build the project itself.
      */
     val dogfoodingVersion: String
-    private const val fallbackDfVersion = "0.69.0"
+    private const val fallbackDfVersion = "0.69.2"
 
     /**
      * The artifact for the ProtoData Gradle plugin.
