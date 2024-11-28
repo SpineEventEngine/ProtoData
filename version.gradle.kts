df9ec7e71f0f947f3985cadf7ad92b8fffbcfd53
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-28 18:06:53 +0000

    Bump version -> `0.69.5`

--- version.gradle.kts
+++ version.gradle.kts
@@ -32,4 +32,4 @@
  *
  * For dependencies on Spine SDK module please see [io.spine.dependency.local.Spine].
  */
-val protoDataVersion: String by extra("0.69.3")
+val protoDataVersion: String by extra("0.69.5")
