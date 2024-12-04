e446246539facadced3506bc3465fb5e08445cea
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-12-04 18:57:54 +0000

    Update `config`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/ModelCompiler.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/ModelCompiler.kt
@@ -24,21 +24,17 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.dependency.lib
+package io.spine.dependency.local
 
 /**
- * The dependencies for Guava.
+ * Spine Model Compiler Gradle API.
  *
- * When changing the version, also change the version used in the `build.gradle.kts`. We need
- * to synchronize the version used in `buildSrc` and in Spine modules. Otherwise, when testing
- * Gradle plugins, errors may occur due to version clashes.
- *
- * @see <a href="https://github.com/google/guava">Guava at GitHub</a>.
+ * @see <a href="https://github.com/SpineEventEngine/model-compiler">spine-model-compiler</a>
  */
-@Suppress("unused", "ConstPropertyName")
-object Guava {
-    private const val version = "32.1.3-jre"
-    const val group = "com.google.guava"
-    const val lib     = "$group:guava:$version"
-    const val testLib = "$group:guava-testlib:$version"
+@Suppress("ConstPropertyName")
+object ModelCompiler {
+    const val version = "2.0.0-SNAPSHOT.133"
+    const val group = Spine.toolsGroup
+    const val artifact = "spine-model-compiler"
+    const val lib = "$group:$artifact:$version"
 }
--- buildSrc/src/main/kotlin/test-module.gradle.kts
+++ buildSrc/src/main/kotlin/test-module.gradle.kts
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import io.spine.dependency.local.Spine
+import io.spine.dependency.local.Base
 import io.spine.dependency.local.Validation
 
 plugins {
@@ -34,7 +34,7 @@ plugins {
 
 dependencies {
     arrayOf(
-        Spine.base,
+        Base.lib,
         Validation.runtime
     ).forEach {
         testFixturesImplementation(it)?.because(
