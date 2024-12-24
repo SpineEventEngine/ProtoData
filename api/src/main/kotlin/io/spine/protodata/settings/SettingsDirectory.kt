0411f842212a027bf61cf277d940748d79bab967
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-12-24 19:15:22 +0000

    Document how the settings directory works with the backend

--- api/src/main/kotlin/io/spine/protodata/settings/SettingsDirectory.kt
+++ api/src/main/kotlin/io/spine/protodata/settings/SettingsDirectory.kt
@@ -34,12 +34,23 @@ import java.nio.file.Path
 import kotlin.io.path.listDirectoryEntries
 
 /**
- * A directory containing settings files.
+ * A directory containing settings files for ProtoData plugins or their parts.
  *
  * Only the files with the [recognized extensions][Format] are considered settings files.
  *
  * Only the files belonging to the directory directly are considered,
  * no subdirectories are traversed.
+ *
+ * This class works in combination with the view which manages the [Settings] state class
+ * on the ProtoData backend. The view is subscribed to the [SettingsFileDiscovered] event for
+ * storing the name of the discovered settings file.
+ *
+ * The method [emitEvents] of this class traverses all the files stored in the settings directory
+ * emitting the [SettingsFileDiscovered] event for each file with the recognized format.
+ *
+ * In order to load settings, ProtoData plugins or their parts should implement
+ * the [LoadsSettings] interface, which provides the discovery and loading methods based
+ * on querying instances of the [Settings] class on the backend.
  *
  * @param path The existing path to the settings directory.
  */
