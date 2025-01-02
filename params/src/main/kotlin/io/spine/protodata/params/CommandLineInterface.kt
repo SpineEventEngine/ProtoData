7d9a26ae77022f87b199ef5d55f435ac6ede35f3
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-02 20:18:26 +0000

    Remove parameter objects

--- params/src/main/kotlin/io/spine/protodata/params/CommandLineInterface.kt
+++ params/src/main/kotlin/io/spine/protodata/params/CommandLineInterface.kt
@@ -28,8 +28,6 @@
 
 package io.spine.protodata.params
 
-import io.spine.protodata.plugin.Plugin
-
 /**
  * The command-line parameter for specifying the name of the file which stores an instance of
  * [PipelineParameters] in [PROTO_JSON][io.spine.protodata.util.Format.PROTO_JSON] format.
@@ -41,47 +39,6 @@ public object ParametersFileParam : Parameter(
         The path to the file with the serialized instance of `PipelineParameters` to 
         be passed to the pipeline. The file must be in `pb.json` format.
     """.trimIndent()
-)
-
-/**
- * The part of the console output used for showing how to use the dollar sign for
- * a nested binary class name.
- *
- * The backslash symbol (`\\`) preceding the dollar sign (`\$)` is used for escaping
- * the dollar in the console input because otherwise it would be considered as
- * a reference to a shell script variable.
- */
-@Suppress("TopLevelPropertyNaming") // for brevity.
-private const val escDollar: String = "\\\$"
-
-/**
- * The command-line parameter for specifying ProtoData plugins used in
- * the code generation process.
- */
-public object PluginParam : Parameter(
-    name = "--plugin",
-    shortName = "-p",
-    help = """
-        The name of a Java or a Kotlin class, a subtype of `${Plugin::class.qualifiedName}`.
-        For nested classes please use binary names, with the `$` delimiter before a nested class.
-        To pass more than one plugin class, type:
-           `<...> ${dash.p} com.foo.FirstPlugin ${dash.p} com.foo.Second${escDollar}NestedPlugin`.
-        """
-)
-
-/**
- * The command-line parameter for composing the user-defined classpath.
- */
-public object UserClasspathParam : Parameter(
-    name = "--user-classpath",
-    shortName = "--ucp",
-    help = """
-        The user classpath which contains all `${ddash.plugin}` classes, as well as all
-        their dependencies, which are not included as a part of the ProtoData library. 
-        This option may be omitted if the classes are already present in the ProtoData classpath. 
-        May be one path to a JAR, a ZIP, or a directory. 
-        Or, may be many paths separated by the system-dependent path separator (`$ps`).
-        """
 )
 
 /**
@@ -105,19 +62,3 @@ public object DebugLoggingParam : Parameter(
         Set log level to `DEBUG`.        
     """
 )
-
-/**
- * Abbreviations for short parameter names to be used inside `help` texts.
- */
-@Suppress("ClassName") // for better readability
-private object dash {
-    val p = lazy { PluginParam.shortName }
-}
-
-/**
- * Abbreviations for long (double-dash) parameter names to be used in `help` texts.
- */
-@Suppress("ClassName", "SpellCheckingInspection") // for better readability in `help` texts.
-private object ddash {
-    val plugin = lazy { PluginParam.name }
-}
