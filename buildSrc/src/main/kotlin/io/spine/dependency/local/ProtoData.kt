aa5bea0cee1c305caedc3566e3797e03f3e2b4d7
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-02-20 20:36:07 +0000

    Bump local dependencies

--- buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
@@ -73,7 +73,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.92.6"
+    private const val fallbackVersion = "0.92.5"
 
     /**
      * The distinct version of ProtoData used by other build tools.
